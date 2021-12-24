5fddfbba7cdb539942a9a02727e546f58decac34
root <root@ip-172-31-14-229.us-east-2.compute.internal>
2021-12-24 11:22:13 +0000

    changed APP.js file content

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Wsp manh :)
         </p>
         <a
           className="App-link"
