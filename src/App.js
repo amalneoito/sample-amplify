20fa1152a5b6692b5a101e684360dfc1f5600050
amalneoito <85620265+amalneoito@users.noreply.github.com>
2021-12-25 15:05:21 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Wsp broh :)
+          Wsp manhh :)
         </p>
         <a
           className="App-link"
