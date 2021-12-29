1f0f8eba85b45db2410be3fa0d8de6eded0dadd7
root <root@ip-172-31-14-229.us-east-2.compute.internal>
2021-12-29 05:17:10 +0000

    updated the App.js file

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         thanks mom and god for the all helps
+         tset branch jenkins working properly!!!
         </p>
         <a
           className="App-link"
